0d00fb6a0ab9880c7afae31efb9e307c99102780
battistaennio <ennio.battista99@gmail.com>
2024-06-03 16:23:00 +0200

    ESERCIZIO TERMINATO

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 // Chiedi all’utente il suo nome 
 
 let userName = prompt("Inserisci il tuo nome");
+document.getElementById("nome").innerHTML= userName;
 
 // chiedi il suo cognome
 
 let userSurname = prompt("Inserisci il tuo cognome");
+document.getElementById("cognome").innerHTML= userSurname;
 
 // chiedi il suo colore preferito
 
 let userFavouriteColor = prompt("Inserisci il tuo colore preferito");
+document.getElementById("colore").innerHTML= userFavouriteColor;
 
 // crea costante per 23
 
@@ -20,4 +23,4 @@ let userPassword = userName + userSurname + userFavouriteColor + numeroFisso;
 
 // scrivi sulla pagina nomecognomecolorepreferito23
 
-document.getElementById("risultato-password").innerHTML = "La tua efficentissima password è: " + userPassword;
+document.getElementById("risultato-password").innerHTML = userPassword;
