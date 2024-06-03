77e7adc7d4994f6afce242c0918f73f56a967b0d
battistaennio <ennio.battista99@gmail.com>
2024-06-03 14:35:10 +0200

    Punto di partenza

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Chiedi all’utente il suo nome 
+
+
+
+// chiedi il suo cognome
+
+
+
+// chiedi il suo colore preferito
+
+
+
+// crea costante per 23
+
+
+
+// aggiungi nome + cognome + colorepreferito + 23
+
+
+
+// scrivi sulla pagina nomecognomecolorepreferito23
